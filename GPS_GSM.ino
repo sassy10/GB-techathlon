802626f2b5590fcc6f37c4a3849c943e06e06564
sassy10 <44094384+sassy10@users.noreply.github.com>
2018-10-14 07:21:31 +0530

    Add files via upload

---
+++ GPS_GSM.ino
@@ -0,0 +1,59 @@
+#include<TinyGPS.h>
+#include<SoftwareSerial.h>
+
+SoftwareSerial Gsm(7,8);
+char phone_no[]="919164560609";
+
+TinyGPS gps;
+
+void setup()
+{
+  Serial.begin(9600);
+  Gsm.begin(9600);
+}
+
+void loop()
+{
+  bool newData=false;
+  unsigned long char;
+  unsigned short sentences,failed;
+
+  for(unsigned long start = millis();millis()-start<1000;)
+  {
+    while(Serial.available())
+    {
+      char c=Serial.read();
+      Serial.print(c);
+      if(gps.encode(c))
+        newData=true;
+    }
+  }
+  if(newData)
+  {
+    float flat,flon;
+    unsigned long age;
+    gps.f_get_position(&flat,&flon,&age);
+    Gsm.print("AT+CMGF=1\r");
+    delay(400);
+    Gsm.print("AT+CMGS=\"");
+    Gsm.print(phone_no);
+    Gsm.println("\"");
+
+    delay(300);
+    Serial.print("Hi");
+    Gsm.print("Hi");
+    Gsm.print("http://maps.google.com/maps?q=loc:");
+    Gsm.print(flat==TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat,6);
+    Serial.print(",");
+    Gsm.print(flon==TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon,6);
+    delay(200);
+    Gsm.println((char)26);
+    delay(200);
+    Gsm.println();
+    delay(20000);
+  }
+
+  Serial.println(failed);
+}
+
+
---
+++ ultrasonic.ino
@@ -0,0 +1,41 @@
+int trigPin=11;
+int echoPin=12;
+long duration,cm,inches;
+int motor=8;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(motor,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+cm = (duration/2) / 29.1; 
+inches = (duration/2) / 74;
+
+
+  if(90>=cm) 
+   analogWrite(motor,100); 
+   else
+   analogWrite(motor,0); 
+ 
+  analogWrite(motor,200);
+Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(250);
+
+}
